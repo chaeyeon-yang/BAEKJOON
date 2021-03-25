55560fa538d712387b4bb05a77ece247d5c49345
chaeyeon-yang <myday0827@naver.com>
2021-03-25 13:00:55 +0900

    210325

--- bronze/io/Problem5.py
+++ bronze/io/Problem5.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 import sys
 a,b=map(int, sys.stdin.readline().split())
 print(a+b)
-=======
-import sys
-a,b=map(int, sys.stdin.readline().split())
-print(a+b)
->>>>>>> c04772ae6fd7e007032a7aaab4dbbd97899770d7

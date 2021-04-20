506598f60acc17e0800023bad1901ad13169e8e5
chaeyeon-yang <myday0827@naver.com>
2021-04-21 00:35:07 +0900

    210421

---
+++ silver/recursive/hanoi.py
@@ -0,0 +1,17 @@
+import sys
+n=int(sys.stdin.readline())
+print(2**n-1)
+def move(start, dest):
+    print(start, dest)
+def run(n,start, mid, dest):
+    if n==1:
+        return move(start, dest)
+    else:
+        run(n-1,start, dest, mid)
+        move(start, dest)
+        run(n-1,mid, start, dest)
+run(n,1,2,3)
+    
+
+
+        
